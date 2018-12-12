95042e0ae4ac9a197752e4caa13fae59581cb65d
waqas <you@example.com>
2018-12-12 14:34:37 +0530

    Initial ISBN check logic

---
+++ validate.py
@@ -0,0 +1,13 @@
+import re
+
+def check(isbn):
+    isbn = isbn.replace("-", "").replace(" ", "").upper()
+    match = re.search(r'^(\d{9})(\d|X)$', isbn)
+    if not match:
+        return False
+
+    digits = match.group(1)
+    check_digit = 10 if match.group(2) == 'X' else int(match.group(2))
+
+    result = sum((i + 1) * int(digit) for i, digit in enumerate(digits))
+    return (result % 11) == check_digit
